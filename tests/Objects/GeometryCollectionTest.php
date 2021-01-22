ad2b8fef637976d51cd9499dff04c9e9b4a587dd
Matan Yadaev <matan.yed@gmail.com>
2021-01-23 00:42:19 +0200

    test feature collection

--- tests/Objects/GeometryCollectionTest.php
+++ tests/Objects/GeometryCollectionTest.php
@@ -94,6 +94,40 @@ class GeometryCollectionTest extends TestCase
     }
 
     /** @test */
+    public function it_stores_geometry_collection_from_feature_collection_geo_json()
+    {
+        /** @var TestPlace $testPlace */
+        $testPlace = TestPlace::factory()->create([
+            'geometry_collection' => GeometryCollection::fromJson('{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[55.5,23.1],[55.6,23.2],[55.7,23.3],[55.5,23.1]]]}},{"type":"Feature","geometry":{"type":"Point","coordinates":[55.5,23.1]}}]}'),
+        ])->fresh();
+
+        $this->assertTrue($testPlace->geometry_collection instanceof GeometryCollection);
+
+        $geometries = $testPlace->geometry_collection->getGeometries();
+        /** @var Polygon $polygon */
+        $polygon = $geometries[0];
+        $lineStrings = $polygon->getGeometries();
+        $points = $lineStrings[0]->getGeometries();
+
+        $this->assertEquals(23.1, $points[0]->latitude);
+        $this->assertEquals(55.5, $points[0]->longitude);
+        $this->assertEquals(23.2, $points[1]->latitude);
+        $this->assertEquals(55.6, $points[1]->longitude);
+        $this->assertEquals(23.3, $points[2]->latitude);
+        $this->assertEquals(55.7, $points[2]->longitude);
+        $this->assertEquals(23.1, $points[3]->latitude);
+        $this->assertEquals(55.5, $points[3]->longitude);
+
+        /** @var Point $point */
+        $point = $geometries[1];
+
+        $this->assertEquals(23.1, $point->latitude);
+        $this->assertEquals(55.5, $point->longitude);
+
+        $this->assertDatabaseCount($testPlace->getTable(), 1);
+    }
+
+    /** @test */
     public function it_generates_multi_polygon_geo_json()
     {
         $geometryCollection = new GeometryCollection([
