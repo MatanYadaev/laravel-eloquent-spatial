c788cf79bd7ae93e0a9b7888bc6f2fae81a81560
Matan Yadaev <matan.yed@gmail.com>
2021-02-08 23:20:04 +0200

    style fix

--- tests/Builders/SpatialBuilderTest.php
+++ tests/Builders/SpatialBuilderTest.php
@@ -4,7 +4,6 @@ namespace MatanYadaev\EloquentSpatial\Tests\Builders;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use MatanYadaev\EloquentSpatial\Objects\LineString;
-use MatanYadaev\EloquentSpatial\Objects\MultiPolygon;
 use MatanYadaev\EloquentSpatial\Objects\Point;
 use MatanYadaev\EloquentSpatial\Objects\Polygon;
 use MatanYadaev\EloquentSpatial\Tests\TestCase;
