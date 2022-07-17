41e8b29d4875b7720505861e070b005b79432b33
Matan Yadaev <matan@zerolimits.io>
2022-07-17 23:09:35 +0300

    Refactor GeometryTest

--- tests/GeometryTest.php
+++ tests/GeometryTest.php
@@ -1,59 +1,45 @@
 <?php
 
-namespace MatanYadaev\EloquentSpatial\Tests;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Support\Facades\DB;
-use InvalidArgumentException;
 use MatanYadaev\EloquentSpatial\Objects\LineString;
 use MatanYadaev\EloquentSpatial\Objects\Point;
-use MatanYadaev\EloquentSpatial\Tests\TestModels\TestPlace;
 
-class GeometryTest extends TestCase
-{
-  use DatabaseMigrations;
+uses(DatabaseMigrations::class);
 
-  /** @test */
-  public function it_throws_exception_when_generating_geometry_from_invalid_value(): void
-  {
-    $this->expectException(InvalidArgumentException::class);
 
+it('throws exception when generating geometry from other geometry WKB', function (): void {
+  expect(function (): void {
+    $pointWkb = '000000000140000000000000004010000000000000'; // POINT
+
+    LineString::fromWkb($pointWkb);
+  })->toThrow(InvalidArgumentException::class);
+});
+
+it('throws exception when generating geometry from other geometry WKT', function (): void {
+  expect(function (): void {
+    $pointWkt = 'POINT(0 180)';
+
+    LineString::fromWkt($pointWkt);
+  })->toThrow(InvalidArgumentException::class);
+});
+
+it('throws exception when generating geometry from non-JSON', function (): void {
+  expect(function (): void {
     Point::fromJson('invalid-value');
-  }
+  })->toThrow(InvalidArgumentException::class);
+});
 
-  /** @test */
-  public function it_throws_exception_when_generating_geometry_from_other_geometry_wkb(): void
-  {
-    $this->expectException(InvalidArgumentException::class);
-
-    TestPlace::insert(array_merge(TestPlace::factory()->definition(), [
-      'point_with_line_string_cast' => DB::raw('POINT(0, 180)'),
-    ]));
-
-    TestPlace::firstOrFail()->getAttribute('point_with_line_string_cast');
-  }
-
-  /** @test */
-  public function it_throws_exception_when_generating_geometry_from_other_geometry_wkt(): void
-  {
-    $this->expectException(InvalidArgumentException::class);
-
-    LineString::fromWkt('POINT(0 180)');
-  }
-
-  /** @test */
-  public function it_throws_exception_when_generating_geometry_from_invalid_geo_json(): void
-  {
-    $this->expectException(InvalidArgumentException::class);
-
+it('throws exception when generating geometry from empty JSON', function (): void {
+  expect(function (): void {
     Point::fromJson('{}');
-  }
+  })->toThrow(InvalidArgumentException::class);
+});
 
-  /** @test */
-  public function it_throws_exception_when_generating_geometry_from_other_geometry_geo_json(): void
-  {
-    $this->expectException(InvalidArgumentException::class);
+it('throws exception when generating geometry from other geometry JSON', function (): void {
+  expect(function (): void {
+    $pointJson = '{"type":"Point","coordinates":[0,180]}';
 
-    Point::fromJson('{"type":"LineString","coordinates":[[0,180],[1,179]]}');
-  }
-}
+    LineString::fromJson($pointJson);
+  })->toThrow(InvalidArgumentException::class);
+});
