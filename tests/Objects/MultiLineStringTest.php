6171b77cc3fceefb59d6d9a05449fb8409535ed2
Matan Yadaev <matan@zerolimits.io>
2022-07-18 23:40:51 +0300

    Refactor MultiLineStringTest

--- tests/Objects/MultiLineStringTest.php
+++ tests/Objects/MultiLineStringTest.php
@@ -1,106 +1,81 @@
 <?php
 
-namespace MatanYadaev\EloquentSpatial\Tests\Objects;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use InvalidArgumentException;
 use MatanYadaev\EloquentSpatial\Objects\LineString;
 use MatanYadaev\EloquentSpatial\Objects\MultiLineString;
 use MatanYadaev\EloquentSpatial\Objects\Point;
-use MatanYadaev\EloquentSpatial\Tests\TestCase;
 use MatanYadaev\EloquentSpatial\Tests\TestModels\TestPlace;
 
-class MultiLineStringTest extends TestCase
-{
-  use DatabaseMigrations;
+uses(DatabaseMigrations::class);
 
-  /** @test */
-  public function it_stores_multi_line_string(): void
-  {
-    /** @var TestPlace $testPlace */
-    $testPlace = TestPlace::factory()->create([
-      'multi_line_string' => new MultiLineString([
-        new LineString([
-          new Point(180, 0),
-          new Point(179, 1),
-        ]),
-      ]),
-    ]);
+it('creates a model record with multi line string', function (): void {
+  $multiLineString = new MultiLineString([
+    new LineString([
+      new Point(180, 0),
+      new Point(179, 1),
+    ]),
+  ]);
 
-    $this->assertTrue($testPlace->multi_line_string instanceof MultiLineString);
+  /** @var TestPlace $testPlace */
+  $testPlace = TestPlace::factory()->create([    'multi_line_string' => $multiLineString  ]);
 
-    $lineString = $testPlace->multi_line_string[0];
+  expect($testPlace->multi_line_string)->toBeInstanceOf(MultiLineString::class);
+  expect($testPlace->multi_line_string)->toEqual($multiLineString);
+});
 
-    $this->assertEquals(180, $lineString[0]->latitude);
-    $this->assertEquals(0, $lineString[0]->longitude);
-    $this->assertEquals(179, $lineString[1]->latitude);
-    $this->assertEquals(1, $lineString[1]->longitude);
+it('creates multi line string from JSON', function (): void {
+  $multiLineString = new MultiLineString([
+    new LineString([
+      new Point(180, 0),
+      new Point(179, 1),
+    ]),
+  ]);
 
-    $this->assertDatabaseCount($testPlace->getTable(), 1);
-  }
+  $multiLineStringFromJson = MultiLineString::fromJson('{"type":"MultiLineString","coordinates":[[[0,180],[1,179]]]}');
 
-  /** @test */
-  public function it_stores_multi_line_string_from_geo_json(): void
-  {
-    /** @var TestPlace $testPlace */
-    $testPlace = TestPlace::factory()->create([
-      'multi_line_string' => MultiLineString::fromJson('{"type":"MultiLineString","coordinates":[[[0,180],[1,179]]]}'),
-    ]);
+  expect($multiLineStringFromJson)->toEqual($multiLineString);
+});
 
-    $this->assertTrue($testPlace->multi_line_string instanceof MultiLineString);
+it('generates multi line string JSON', function (): void {
+  $multiLineString = new MultiLineString([
+    new LineString([
+      new Point(180, 0),
+      new Point(179, 1),
+    ]),
+  ]);
 
-    $lineString = $testPlace->multi_line_string[0];
+  $json = $multiLineString->toJson();
 
-    $this->assertEquals(180, $lineString[0]->latitude);
-    $this->assertEquals(0, $lineString[0]->longitude);
-    $this->assertEquals(179, $lineString[1]->latitude);
-    $this->assertEquals(1, $lineString[1]->longitude);
+  $expectedJson = '{"type":"MultiLineString","coordinates":[[[0,180],[1,179]]]}';
+  expect($json)->toBe($expectedJson);
+});
 
-    $this->assertDatabaseCount($testPlace->getTable(), 1);
-  }
+it('generates multi line string feature collection JSON', function (): void {
+  $multiLineString = new MultiLineString([
+    new LineString([
+      new Point(180, 0),
+      new Point(179, 1),
+    ]),
+  ]);
 
-  /** @test */
-  public function it_generates_multi_line_string_geo_json(): void
-  {
-    $multiLineString = new MultiLineString([
-      new LineString([
-        new Point(180, 0),
-        new Point(179, 1),
-      ]),
-    ]);
+  $featureCollectionJson = $multiLineString->toFeatureCollectionJson();
 
-    $this->assertEquals('{"type":"MultiLineString","coordinates":[[[0,180],[1,179]]]}', $multiLineString->toJson());
-  }
+  $expectedFeatureCollectionJson = '{"type":"FeatureCollection","features":[{"type":"Feature","properties":[],"geometry":{"type":"MultiLineString","coordinates":[[[0,180],[1,179]]]}}]}';
+  expect($featureCollectionJson)->toBe($expectedFeatureCollectionJson);
+});
 
-  /** @test */
-  public function it_generates_multi_line_string_feature_collection_json(): void
-  {
-    $multiLineString = new MultiLineString([
-      new LineString([
-        new Point(180, 0),
-        new Point(179, 1),
-      ]),
-    ]);
-
-    $this->assertEquals('{"type":"FeatureCollection","features":[{"type":"Feature","properties":[],"geometry":{"type":"MultiLineString","coordinates":[[[0,180],[1,179]]]}}]}', $multiLineString->toFeatureCollectionJson());
-  }
-
-  /** @test */
-  public function it_throws_exception_when_multi_line_string_has_0_line_strings(): void
-  {
-    $this->expectException(InvalidArgumentException::class);
-
+it('throws exception when multi line string has no line strings', function (): void {
+  expect(function (): void {
     new MultiLineString([]);
-  }
+  })->toThrow(InvalidArgumentException::class);
+});
 
-  /** @test */
-  public function it_throws_exception_when_multi_line_string_has_composed_by_point(): void
-  {
-    $this->expectException(InvalidArgumentException::class);
-
+it('throws exception when creating multi line string from incorrect geometry', function (): void {
+  expect(function (): void {
     // @phpstan-ignore-next-line
     new MultiLineString([
       new Point(0, 0),
     ]);
-  }
-}
+  })->toThrow(InvalidArgumentException::class);
+});
