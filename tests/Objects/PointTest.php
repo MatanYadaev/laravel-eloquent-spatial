7d01cae67745a37c8aafc20b27612baae20add86
Matan Yadaev <matan@zerolimits.io>
2022-07-19 23:41:13 +0300

    Add tests to Point

--- tests/Objects/PointTest.php
+++ tests/Objects/PointTest.php
@@ -16,12 +16,61 @@ it('creates a model record with point', function (): void {
   expect($testPlace->point)->toEqual($point);
 });
 
+it('creates a model record with point with SRID', function (): void {
+  $point = new Point(180, 0, 4326);
+
+  /** @var TestPlace $testPlace */
+  $testPlace = TestPlace::factory()->create(['point' => $point]);
+
+  expect($testPlace->point->srid)->toEqual(4326);
+});
+
 it('creates point from JSON', function (): void {
   $point = new Point(180, 0);
 
   $pointFromJson = Point::fromJson('{"type":"Point","coordinates":[0,180]}');
 
   expect($pointFromJson)->toEqual($point);
+});
+
+it('creates point with SRID from JSON', function (): void {
+  $point = new Point(180, 0, 4326);
+
+  $pointFromJson = Point::fromJson('{"type":"Point","coordinates":[0,180]}', 4326);
+
+  expect($pointFromJson)->toEqual($point);
+});
+
+it('creates point from WKT', function (): void {
+  $point = new Point(180, 0);
+
+  $pointFromWkt = Point::fromWkt('POINT(0 180)');
+
+  expect($pointFromWkt)->toEqual($point);
+});
+
+it('creates point with SRID from WKT', function (): void {
+  $point = new Point(180, 0, 4326);
+
+  $pointFromWkt = Point::fromWkt('POINT(0 180)', 4326);
+
+  expect($pointFromWkt)->toEqual($point);
+});
+
+it('creates point from WKB', function (): void {
+  $point = new Point(180, 0);
+
+  $pointFromWkb = Point::fromWkb($point->toWkb());
+
+  expect($pointFromWkb)->toEqual($point);
+});
+
+it('creates point with SRID from WKB', function (): void {
+  $point = new Point(180, 0, 4326);
+
+  $pointFromWkb = Point::fromWkb($point->toWkb(), 4326);
+
+  expect($pointFromWkb)->toEqual($point);
 });
 
 it('generates point json', function (): void {
