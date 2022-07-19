b03fdbe060bbb659fe6973695faf301addddbae7
Matan Yadaev <matan@zerolimits.io>
2022-07-19 23:52:41 +0300

    Add tests to Polygon

--- tests/Objects/PolygonTest.php
+++ tests/Objects/PolygonTest.php
@@ -26,6 +26,23 @@ it('creates a model record with polygon', function (): void {
   expect($testPlace->polygon)->toEqual($polygon);
 });
 
+it('creates a model record with polygon with SRID', function (): void {
+  $polygon = new Polygon([
+    new LineString([
+      new Point(180, 0),
+      new Point(179, 1),
+      new Point(178, 2),
+      new Point(177, 3),
+      new Point(180, 0),
+    ]),
+  ], 4326);
+
+  /** @var TestPlace $testPlace */
+  $testPlace = TestPlace::factory()->create(['polygon' => $polygon]);
+
+  expect($testPlace->polygon->srid)->toBe(4326);
+});
+
 it('creates polygon from JSON', function (): void {
   $polygon = new Polygon([
     new LineString([
@@ -38,6 +55,22 @@ it('creates polygon from JSON', function (): void {
   ]);
 
   $polygonFromJson = Polygon::fromJson('{"type":"Polygon","coordinates":[[[0,180],[1,179],[2,178],[3,177],[0,180]]]}');
+
+  expect($polygonFromJson)->toEqual($polygon);
+});
+
+it('creates polygon with SRID from JSON', function (): void {
+  $polygon = new Polygon([
+    new LineString([
+      new Point(180, 0),
+      new Point(179, 1),
+      new Point(178, 2),
+      new Point(177, 3),
+      new Point(180, 0),
+    ]),
+  ], 4326);
+
+  $polygonFromJson = Polygon::fromJson('{"type":"Polygon","coordinates":[[[0,180],[1,179],[2,178],[3,177],[0,180]]]}', 4326);
 
   expect($polygonFromJson)->toEqual($polygon);
 });
@@ -74,6 +107,70 @@ it('generates polygon feature collection JSON', function (): void {
 
   $expectedFeatureCollectionJson = '{"type":"FeatureCollection","features":[{"type":"Feature","properties":[],"geometry":{"type":"Polygon","coordinates":[[[0,180],[1,179],[2,178],[3,177],[0,180]]]}}]}';
   expect($featureCollectionJson)->toBe($expectedFeatureCollectionJson);
+});
+
+it('creates polygon from WKT', function (): void {
+  $polygon = new Polygon([
+    new LineString([
+      new Point(180, 0),
+      new Point(179, 1),
+      new Point(178, 2),
+      new Point(177, 3),
+      new Point(180, 0),
+    ]),
+  ]);
+
+  $polygonFromWkt = Polygon::fromWkt('POLYGON((0 180,1 179,2 178,3 177,0 180))');
+
+  expect($polygonFromWkt)->toEqual($polygon);
+});
+
+it('creates polygon with SRID from WKT', function (): void {
+  $polygon = new Polygon([
+    new LineString([
+      new Point(180, 0),
+      new Point(179, 1),
+      new Point(178, 2),
+      new Point(177, 3),
+      new Point(180, 0),
+    ]),
+  ], 4326);
+
+  $polygonFromWkt = Polygon::fromWkt('POLYGON((0 180,1 179,2 178,3 177,0 180))', 4326);
+
+  expect($polygonFromWkt)->toEqual($polygon);
+});
+
+it('creates polygon from WKB', function (): void {
+  $polygon = new Polygon([
+    new LineString([
+      new Point(180, 0),
+      new Point(179, 1),
+      new Point(178, 2),
+      new Point(177, 3),
+      new Point(180, 0),
+    ]),
+  ]);
+
+  $polygonFromWkb = Polygon::fromWkb($polygon->toWkb());
+
+  expect($polygonFromWkb)->toEqual($polygon);
+});
+
+it('creates polygon with SRID from WKB', function (): void {
+  $polygon = new Polygon([
+    new LineString([
+      new Point(180, 0),
+      new Point(179, 1),
+      new Point(178, 2),
+      new Point(177, 3),
+      new Point(180, 0),
+    ]),
+  ], 4326);
+
+  $polygonFromWkb = Polygon::fromWkb($polygon->toWkb(), 4326);
+
+  expect($polygonFromWkb)->toEqual($polygon);
 });
 
 it('throws exception when polygon has no line strings', function (): void {
