09c4704b76f71f24a3b891bde58e0b4798dd4ee0
Riley Schoppa <rschoppa@wastequip.com>
2023-02-28 18:17:19 -0500

    Add test for distance

--- tests/GeometryUtilitiesTest.php
+++ tests/GeometryUtilitiesTest.php
@@ -22,7 +22,7 @@ it('creates a convex hull', function (): void {
   expect($hull->toArray()['coordinates'][0])->toEqualCanonicalizing($expectedPolygon['coordinates'][0]);
 });
 
-it('calculates the distance between points on a sphere with sphere size', function (): void {
+it('calculates the distance between points on a sphere', function (): void {
 
   $point1 = new Point(41.9631174, -87.6770458);
   $point2 = new Point(40.7628267, -73.9898293);
@@ -31,4 +31,15 @@ it('calculates the distance between points on a sphere with sphere size', functi
     ->distanceSphere($point1, $point2);
 
   expect($distance)->toBe(1148798.720296128);
+});
+
+it('calculates the distance between points on a sphere with sphere size', function (): void {
+
+  $point1 = new Point(41.9631174, -87.6770458);
+  $point2 = new Point(40.7628267, -73.9898293);
+
+  $distance = GeometryUtilities::make()
+    ->distanceSphere($point1, $point2, 1);
+
+  expect($distance)->toBe(0.18031725706132895);
 });
