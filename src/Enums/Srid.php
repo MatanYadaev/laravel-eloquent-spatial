35c483b7025e29e306d9059fde2089ba6d4ec9d4
Riley Schoppa <rschoppa@wastequip.com>
2023-02-28 16:10:03 -0500

    Remove doc block

--- src/Enums/Srid.php
+++ src/Enums/Srid.php
@@ -2,12 +2,6 @@
 
 namespace MatanYadaev\EloquentSpatial\Enums;
 
-/**
- * Spatial reference identifiers identify the type of coordinate system to use.
- *
- * This includes the most common ones listed on wikipedia
- * Sourced from https://en.wikipedia.org/wiki/Spatial_reference_system
- */
 enum Srid: int
 {
   case WGS84 = 4326; // https://epsg.org/crs_4326/WGS-84.html
