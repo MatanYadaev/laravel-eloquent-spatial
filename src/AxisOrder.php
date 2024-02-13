da2ec56f924827405cf5531af10f57a061d45dab
Rubén Robles <me@d8vjork.com>
2024-02-13 10:29:57 +0100

    fix AxisOrder incompatible with Postgres (PostGIS)

--- src/AxisOrder.php
+++ src/AxisOrder.php
@@ -4,44 +4,49 @@ declare(strict_types=1);
 
 namespace MatanYadaev\EloquentSpatial;
 
+use Illuminate\Database\Connection;
 use Illuminate\Database\ConnectionInterface;
 use Illuminate\Database\MySqlConnection;
 use PDO;
 
 class AxisOrder
 {
-  public function __construct()
-  {
-  }
-
-  public function supported(ConnectionInterface $connection): bool
-  {
-    /** @var MySqlConnection $connection */
-    if ($this->isMariaDb($connection)) {
-      // @codeCoverageIgnoreStart
-      return false;
-      // @codeCoverageIgnoreEnd
+    public function __construct()
+    {
     }
 
-    if ($this->isMySql57($connection)) {
-      // @codeCoverageIgnoreStart
-      return false;
-      // @codeCoverageIgnoreEnd
+    public function supported(ConnectionInterface $connection): bool
+    {
+        if (! $connection instanceof MySqlConnection) {
+          return false;
+        }
+
+        /** @var MySqlConnection $connection */
+        if ($this->isMariaDb($connection)) {
+            // @codeCoverageIgnoreStart
+            return false;
+            // @codeCoverageIgnoreEnd
+        }
+
+        if ($this->isMySql57($connection)) {
+            // @codeCoverageIgnoreStart
+            return false;
+            // @codeCoverageIgnoreEnd
+        }
+
+        return true;
     }
 
-    return true;
-  }
+    private function isMariaDb(MySqlConnection $connection): bool
+    {
+        return $connection->isMaria();
+    }
 
-  private function isMariaDb(MySqlConnection $connection): bool
-  {
-    return $connection->isMaria();
-  }
+    private function isMySql57(MySqlConnection $connection): bool
+    {
+        /** @var string $version */
+        $version = $connection->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION);
 
-  private function isMySql57(MySqlConnection $connection): bool
-  {
-    /** @var string $version */
-    $version = $connection->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION);
-
-    return version_compare($version, '5.8.0', '<');
-  }
+        return version_compare($version, '5.8.0', '<');
+    }
 }
