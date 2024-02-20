80c4f61e140aa83f28e186fe5d3b78b802c1e459
Matan Yadaev <matan.yed@gmail.com>
2024-02-20 09:44:28 +0200

    fix mariadb

--- src/AxisOrder.php
+++ src/AxisOrder.php
@@ -18,11 +18,24 @@ class AxisOrder
 
   public function supported(ConnectionInterface $connection): bool
   {
+    if ($this->isMariaDb($connection)) {
+      return false;
+    }
+
     if ($this->isMySql8OrAbove($connection)) {
       return true;
     }
 
     return false;
+  }
+
+  private function isMariaDb(ConnectionInterface $connection): bool
+  {
+    if (! ($connection instanceof MySqlConnection)) {
+      return false;
+    }
+
+    return $connection->isMaria();
   }
 
   private function isMySql8OrAbove(ConnectionInterface $connection): bool
