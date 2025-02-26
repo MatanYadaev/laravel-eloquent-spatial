571f45d8fb47dade6d069c91661c0558668ec8d3
Matan Yadaev <matan.yed@gmail.com>
2025-02-26 17:49:48 +0200

    Laravel 12 upgrade (#136)
    * laravel 12 upgrade
    
    * fixes
    
    * fixes
    
    * fix phpstan

--- src/Enums/Srid.php
+++ src/Enums/Srid.php
@@ -5,5 +5,5 @@ namespace MatanYadaev\EloquentSpatial\Enums;
 enum Srid: int
 {
     case WGS84 = 4326; // https://epsg.org/crs_4326/WGS-84.html
-    case WEB_MERCATOR = 3857; //https://epsg.org/crs_3857/WGS-84-Pseudo-Mercator.html
+    case WEB_MERCATOR = 3857; // https://epsg.org/crs_3857/WGS-84-Pseudo-Mercator.html
 }
